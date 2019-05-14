619995c12e6709a1cb1a9205614acb9f26db426f
Nonso Amadi <nonsoamadi@aol.com>
2019-05-14 17:05:35 +0100

    Add Filter NavBar

---
+++ UI/js/filter.js
@@ -0,0 +1,39 @@
+const filterOptions = document.getElementById('filter-options')
+const option = document.querySelector('.option');
+
+
+const changeOption =()=>{
+ if(filterOptions.value == "price"){
+  option.innerHTML = ` <input type="text" placeholder="min-price" id="min-price" required> <input type="text" placeholder="max-price" id="max-price" required> `
+ }
+ if(filterOptions.value == "status"){
+  option.innerHTML = `<select name="" id="filter-status" class="filter-options">
+  <option value="-1">Select a Car Status</option>
+  <option value="new">New</option>
+  <option value="used">Used</option>
+ </select>`
+ }
+
+ if(filterOptions.value =="manufacturer"){
+  option.innerHTML = `<select name="" id="filter-manufacturer">
+  <option selected value="0">Select Manufacturer</option>
+  <option value="Audi">Audi</option>
+  <option value="BMW">BMW</option>
+  <option value="Citroen">Citroen</option>
+ <option value="Ford">Ford</option>
+ <option value="Honda">Honda</option>
+ <option value="Jaguar">Jaguar</option>
+ <option value="LandRover">Land Rover</option>
+ <option value="Mercedes">Mercedes</option>
+ <option value="Mini">Mini</option>
+ <option value="Nissan">Nissan</option>
+ <option value="Toyota">Toyota</option>
+ <option value="Volvo">Volvo</option>
+ </select>`
+ }
+ if(filterOptions.value == '-1'){
+ option.innerHTML = 'Please select a filter option'
+ }
+}
+
+filterOptions.addEventListener('change', changeOption)
