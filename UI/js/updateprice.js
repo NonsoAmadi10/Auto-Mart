62849e5e95b12fbe1b17f61d20f97ded72833e40
Nonso Amadi <nonsoamadi@aol.com>
2019-05-14 03:46:51 +0100

    (feature): Implement User should update car sale advertisement - Build Out the UI - Simulate modal Events with Javascript - Simulate Update Price function with Javascript
    [Delivers #165989795]

---
+++ UI/js/updateprice.js
@@ -0,0 +1,23 @@
+const price = document.getElementById('price');
+const status= document.getElementById('update-status');
+const updateForm = document.getElementById('update-form');
+
+
+const update=(e)=>{
+ e.preventDefault();
+ const regNewprice = document.getElementById('new-price').value.replace(',','');
+ const newPrice = Number(regNewprice);
+ 
+ const newStatus = document.getElementById('status').value;
+ price.innerHTML = `<strong> PRICE: ${newPrice.toFixed(2)}</strong`;
+ if(newStatus !== '0'){
+  status.innerHTML = `<strong> Status: ${newStatus}</strong>`;
+ }else{
+  status.innerHTML = `<strong> Status: </strong> Available`;
+ }
+ modal.style.display= "none";
+
+ 
+}
+
+updateForm.addEventListener('submit', update);
