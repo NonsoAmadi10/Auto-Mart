d951a70d059fcc7b993e1d1eacba35f5e82e5946
Nonso Amadi <nonsoamadi@aol.com>
2019-06-06 01:14:00 +0100

    release(api): deploy to heroku
    [Delivers #166432191]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import babelpolyfill from '@babel/polyfill';
 import userRoutes from './routes/user.route';
 import carRoutes from './routes/cars.route';
 import orderRoutes from './routes/orders.route';
