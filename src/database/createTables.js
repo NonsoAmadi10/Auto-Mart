7d76b8375317d03e7a395dc4e1d019e694fe35aa
Nonso Amadi <nonsoamadi@aol.com>
2019-06-13 12:20:54 +0100

    chore(db-setup): setup database connection - install pg - setup different database for different environment - create database schema
    [Delivers #166616963]

---
+++ src/database/createTables.js
@@ -0,0 +1,49 @@
+const createUsers = 
+`CREATE TABLE IF NOT EXISTS users (
+   id SERIAL PRIMARY KEY,
+   firstname VARCHAR(50) NOT NULL,
+   lastname VARCHAR(50) NOT NULL,
+   email VARCHAR(50) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   address VARCHAR(255) NOT NULL
+);
+`;
+
+const createCars = 
+`CREATE TABLE IF NOT EXISTS cars (
+    id SERIAL PRIMARY KEY,
+    owner INTEGER REFERENCES users(id),
+    createdon TIMESTAMP NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    price NUMERIC NOT NULL,
+    manufacturer VARCHAR(155) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    body_type VARCHAR(50) NOT NULL,
+    flagged BOOLEAN 
+);
+`;
+
+const createOrders = 
+` CREATE TABLE IF NOT EXISTS orders (
+ id SERIAL PRIMARY KEY,
+ car_id INTEGER REFERENCES cars(id),
+ buyer_id INTEGER REFERENCES users(id),
+ amount NUMERIC,
+ status CHAR(50)
+);
+`;
+
+const createFlags =  
+` CREATE TABLE IF NOT EXISTS flags (
+ id SERIAL PRIMARY KEY,
+ car_id INTEGER REFERENCES users(id),
+ reason VARCHAR(255) NOT NULL,
+ description VARCHAR(255) NOT NULL,
+ createdon TIMESTAMP NOT NULL
+);
+`;
+
+const createQuery = `${createUsers}${createCars}${createOrders}${createFlags}`;
+
+export default createQuery;
