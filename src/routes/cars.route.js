83a47a3e98a7e9bbd0f5983f3bc1236202d53593
Nonso Amadi <nonsoamadi@aol.com>
2019-07-15 14:28:26 +0100

    Fix api issue

--- src/routes/cars.route.js
+++ src/routes/cars.route.js
@@ -4,11 +4,11 @@ import { Router } from 'express';
 import authorize from '../middlewares/token';
 import CarAdvertControllers from '../controllers/cars.controllers';
 import upload from '../config/mutler';
-import cloudinaryUploader from '../middlewares/cloudinary'
+//import cloudinaryUploader from '../middlewares/cloudinary'
 
 const router = Router();
 
-router.post('/', authorize, upload.array('image_url', 2), cloudinaryUploader, CarAdvertControllers.postAdvert  );
+router.post('/', authorize, upload.array('image_url', 2), CarAdvertControllers.postAdvert  );
 router.patch('/:id/status', authorize, CarAdvertControllers.updateCarStatusController );
 router.patch('/:id/price', authorize, CarAdvertControllers.UpdateCarPriceController);
 router.get('/:id', authorize, CarAdvertControllers.getSpecificCarController);
