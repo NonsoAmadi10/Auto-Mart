18c6a77364cb3b1691e973851e45f2a56ed61aad
Nonso Amadi <nonsoamadi@aol.com>
2019-07-15 14:58:43 +0100

    Fix patch car price api endpoint

--- src/middlewares/Sanitize.js
+++ src/middlewares/Sanitize.js
@@ -101,7 +101,7 @@ class Sanitize {
     const response = (error, code) => res.status(code).send({ status: 'error', error });
     if (Validator.checkEmpty(price)) return response('Please enter price', 422);
     // eslint-disable-next-line no-restricted-globals
-    //if (isNaN(id)) return response('Invalid URL parameter', 400);
+    if (isNaN(id)) return response('Invalid URL parameter', 400);
     //if (isNaN(price)) return response('Enter a valid price number', 422);
 
     return next();
