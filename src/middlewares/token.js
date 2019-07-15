ddf0352664c3fd94515ca895836a66fd430125b2
Nonso Amadi <nonsoamadi@aol.com>
2019-07-15 15:57:06 +0100

    Fix patch car price api endpoint

--- src/middlewares/token.js
+++ src/middlewares/token.js
@@ -4,12 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const authorize = (req, res, next) => {
-  const bearer = req.headers.authorization;
-  bearer.split(',');
-  const newToken = bearer[2];
+  
   
   const { token } = req.body;
-  if (!token || !newToken) res.status(401).send({ status: 'error', error: 'You must be logged in to use this route' });
+  if (!token) res.status(401).send({ status: 'error', error: 'You must be logged in to use this route' });
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
   
