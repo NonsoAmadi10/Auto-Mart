918155cae1ba0e4572062fdf754e12351de39763
Nonso Amadi <nonsoamadi@aol.com>
2019-07-15 14:49:37 +0100

    Fix api issue  for patch car advert price

--- src/routes/cars.route.js
+++ src/routes/cars.route.js
@@ -1,6 +1,6 @@
 
 import { Router } from 'express';
-//import Sanitize from '../middlewares/Sanitize';
+import Sanitize from '../middlewares/Sanitize';
 import authorize from '../middlewares/token';
 import CarAdvertControllers from '../controllers/cars.controllers';
 import upload from '../config/mutler';
@@ -10,7 +10,7 @@ const router = Router();
 
 router.post('/', authorize, upload.array('image_url', 2), CarAdvertControllers.postAdvert  );
 router.patch('/:id/status', authorize, CarAdvertControllers.updateCarStatusController );
-router.patch('/:id/price', authorize, CarAdvertControllers.UpdateCarPriceController);
+router.patch('/:id/price', authorize, Sanitize.updatePriceSanitizer, CarAdvertControllers.UpdateCarPriceController);
 router.get('/:id', authorize, CarAdvertControllers.getSpecificCarController);
 router.get('/', authorize, CarAdvertControllers.getCarsController);
 router.delete('/:id', authorize, CarAdvertControllers.deleteCarController); 
